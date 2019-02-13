6b8be2e397062a38b1af62c0700fb7ada149094f
Kibeom Kim <31353801+carrotkr@users.noreply.github.com>
2019-02-13 01:41:01 -0800

    Update vis1.js

--- vis1.js
+++ vis1.js
@@ -6,7 +6,7 @@ d3.select('svg')
     .style('width', WIDTH)
     .style('height', HEIGHT);
 
-// Fetch data
+// Fetch data.
 d3.csv('Police.csv').then(function(csv_data) {
     var tempData = d3.nest()
       .key(function(d) { return d.IncidentDayOfWeek; })
@@ -40,7 +40,6 @@ d3.csv('Police.csv').then(function(csv_data) {
     // Set the domain of yScale from yMin and yMax.
     yScale.domain([yMin-1451, yMax]);
 
-    // Find all rectangles.
     d3.selectAll('rect')
         .attr('height', function(datum, index){
         // Set the height of each rectangle by getting the count property of each datum
@@ -53,7 +52,7 @@ d3.csv('Police.csv').then(function(csv_data) {
 
     var xScale = d3.scaleLinear(); // Create the xScale.
     xScale.range([0, WIDTH]); // Set the range to 0->800.
-    xScale.domain([0, tempData.length]); // Set the domain from 0 to the number of data elements retrieved
+    xScale.domain([0, tempData.length]); // Set the domain from 0 to the number of data elements retrieved.
     
     d3.selectAll('rect') // Select all rectangles.
         .attr('x', function(datum, index){
@@ -62,38 +61,53 @@ d3.csv('Police.csv').then(function(csv_data) {
             return xScale(index);
         });
 
-    d3.selectAll('rect') // Select all rectangles.
+    d3.selectAll('rect')
         .attr('y', function(datum, index){
             // Set the y position of each rectangle
-            //  by converting the count property of the datum to a visual value
+            //  by converting the count property of the datum to a visual value.
             return yScale(datum.value);
         });
 
-    d3.selectAll('rect') //select all rectangles
-        .attr('width', WIDTH/tempData.length); //set the width of all rectangles to be the width of the SVG divided by the number of data elements
-    var yDomain = d3.extent(tempData, function(datum, index){ //set the y domain by getting the min/max with d3.extent
-        return datum.value; //... and examining the count property of each datum
+    //-----Making the width of the bars dynamic.-----//
+
+    // Set the width of all rectangles to be the width of the SVG divided by the number of data elements.
+    d3.selectAll('rect')
+        .attr('width', WIDTH/tempData.length);
+
+    //-----Changing the color of the bar based on data.-----//
+
+    var yDomain = d3.extent(tempData, function(datum, index) {
+        // Set the y domain by getting the min/max
+        //  and examining the count property of each datum.
+        return datum.value;
     })
-    var colorScale = d3.scaleLinear();//create a linear scale
-    colorScale.domain(yDomain) //the domain is the yDomain
-    colorScale.range(['#00cc00', 'blue']) //the visual range goes from green->blue
-    d3.selectAll('rect') //select all rectangles
-        .attr('fill', function(datum, index){ //set the fill of each rectangle
-            return colorScale(datum.value) //by converting the count property of the datum to a color
+    var colorScale = d3.scaleLinear(); // Create a linear scale.
+    colorScale.domain(yDomain)
+    colorScale.range(['#00cc00', 'blue']) // The visual range goes from green->blue.
+    d3.selectAll('rect')
+        .attr('fill', function(datum, index) {
+            // Set the fill of each rectangle
+            //  by converting the count property of the datum to a color.
+            return colorScale(datum.value)
         })
-    var leftAxis = d3.axisLeft(yScale); //create a left axis generator using the yScale
-    d3.select('svg') //select the svg
-        .append('g').attr('id', 'left-axis') //append a <g> tag to it with id=left-axis
-        .call(leftAxis); // create a left axis within that <g>
-    var skillScale = d3.scaleBand(); //create a scale band that will map skills to horizontal positions
-    var skillDomain = tempData.map(function(skill){ //create an array of skill strings
+    
+    //-----Adding axes.-----//
+
+    var leftAxis = d3.axisLeft(yScale); // Create a left axis generator using the yScale.
+    d3.select('svg')
+        .append('g').attr('id', 'left-axis')
+        .call(leftAxis);
+    
+    var skillScale = d3.scaleBand(); // Map skills to horizontal positions.
+    var skillDomain = tempData.map(function(skill) { // Create an array of skill strings.
         return skill.key
     });
-    skillScale.range([0, WIDTH]); //set the range of the skillScale to 0->800
-    skillScale.domain(skillDomain); //set the domain to be the array of skill strings
-    var bottomAxis = d3.axisBottom(skillScale); //create a bottom axis generator that uses the skillScale
-    d3.select('svg') //select the svg
-        .append('g').attr('id', 'bottom-axis') //append a <g> tag to it with id=bottom-axis
-        .call(bottomAxis) // create a bottom axis within that <g>
-        .attr('transform', 'translate(0,'+HEIGHT+')'); //move it to the bottom of the svg
+    skillScale.range([0, WIDTH]); // Set the range of the skillScale to 0->800.
+    skillScale.domain(skillDomain); // Set the domain to be the array of skill strings.
+    
+    var bottomAxis = d3.axisBottom(skillScale); // Create a bottom axis generator that uses the skillScale.
+    d3.select('svg')
+        .append('g').attr('id', 'bottom-axis')
+        .call(bottomAxis)
+        .attr('transform', 'translate(0,'+HEIGHT+')'); // Move it to the bottom of the svg.
 });
