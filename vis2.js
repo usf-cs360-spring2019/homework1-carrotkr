d4ffb1a3d7e50ed0e89f7c75d0d16a28ba000ba9
Kibeom Kim <31353801+carrotkr@users.noreply.github.com>
2019-02-13 22:15:08 -0800

    Update vis2.js

--- vis2.js
+++ vis2.js
@@ -91,7 +91,7 @@ d3.csv('Police.csv').then(function(csv_data) {
     var colorScale = d3.scaleLinear(); // Create a linear scale.
     colorScale.domain(yDomain)
     // colorScale.range(['#00cc00', 'blue']) // The visual range goes from green->blue.
-    colorScale.range(['#bbbbbb', '#595959'])
+    colorScale.range(['#9d6a41', '#4d3623'])
     
     d3.selectAll('rect')
         .attr('fill', function(datum, index) {
