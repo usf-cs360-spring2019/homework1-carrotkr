3d9f8cd9136b7ab4e42bb51abc035ff6a02295c5
Kibeom Kim <31353801+carrotkr@users.noreply.github.com>
2019-02-14 03:12:13 -0800

    Update vis3.js

--- vis3.js
+++ vis3.js
@@ -3,14 +3,14 @@ var w = 800;
 var h = 600;
 
 d3.csv('Police.csv').then(function(csv_data) {
-    var tempData = d3.nest()
-      .key(function(d) { return d.IncidentDayOfWeek; })
-      .rollup(function(v) { return v.length; })
-      .entries(csv_data);
+  var tempData = d3.nest()
+    .key(function(d) { return d.IncidentDayOfWeek; })
+    .rollup(function(v) { return v.length; })
+    .entries(csv_data);
 
-    console.log(tempData);
+  console.log(tempData);
 
-    console.log(tempData[1].value);
+  console.log(tempData[1].value);
     var dataset =[]
     console.log(tempData.length);
     var i;
@@ -23,27 +23,27 @@ d3.csv('Police.csv').then(function(csv_data) {
         day.push(tempData[i].key)
     }
 
-            var outerRadius = w / 3;
-            var innerRadius = w / 4;
-            var arc = d3.arc()
-                        .innerRadius(innerRadius)
-                        .outerRadius(outerRadius);
+    var outerRadius = w / 3;
+    var innerRadius = w / 4;
+    var arc = d3.arc()
+      .innerRadius(innerRadius)
+      .outerRadius(outerRadius);
             
-            var pie = d3.pie();
+    var pie = d3.pie();
             
-            //Easy colors accessible via a 10-step ordinal scale
-            var color = d3.scaleOrdinal(d3.schemeCategory10);
+    //Easy colors accessible via a 10-step ordinal scale
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-            //Create SVG element
-            var svg = d3.select("body")
-                        .append("svg")
-                        .attr("width", w)
-                        .attr("height", h);
+    //Create SVG element
+    var svg = d3.select("body")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
 
     //         var container = d3.select('g') //adjust the position of the <g> so it's in the center of the SVG
     // .attr('transform', 'translate(' + (w / 3) + ',' + (h / 3) + ')');
             
-            //Set up groups
+    // Set up groups.
             var arcs = svg.selectAll("g.arc")
                           .data(pie(dataset))
                           .enter()
@@ -83,6 +83,4 @@ d3.csv('Police.csv').then(function(csv_data) {
       .style("fill", "Purple")
       .style("font", "bold 12px Arial")
       .text(function(d, i) { return tempData[i].key; }); //get the label from our original data array
-});
-                        
-    
+}); 
