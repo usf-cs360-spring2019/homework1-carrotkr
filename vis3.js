ea2423c6518124f2d43ad530b2174603897a68e7
Kibeom Kim <31353801+carrotkr@users.noreply.github.com>
2019-02-14 10:59:09 -0800

    Update vis3.js

--- vis3.js
+++ vis3.js
@@ -4,8 +4,8 @@ var h = 600;
 
 // Set width and height of svg.
 d3.select('svg')
-    .style('width', w)
-    .style('height', h);
+  .style('width', w)
+  .style('height', h);
 
 d3.csv('Police.csv').then(function(csv_data) {
   var tempData = d3.nest()
@@ -20,7 +20,6 @@ d3.csv('Police.csv').then(function(csv_data) {
   console.log(tempData.length);
 
   var i;
-  
   for (i = 0; i < tempData.length; i++) {
         dataset.push(tempData[i].value)
   }
@@ -31,65 +30,63 @@ d3.csv('Police.csv').then(function(csv_data) {
         day.push(tempData[i].key)
   }
 
-    var outerRadius = w / 3;
-    var innerRadius = w / 4;
-    var arc = d3.arc()
-      .innerRadius(innerRadius)
-      .outerRadius(outerRadius);
+  var outerRadius = w / 3;
+  // var innerRadius = w / 4;
+  var arc = d3.arc()
+    // .innerRadius(innerRadius)
+    .outerRadius(outerRadius);
             
-    var pie = d3.pie();
+  var pie = d3.pie();
             
-    //Easy colors accessible via a 10-step ordinal scale
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
+  // Colors accessible via a 10-step ordinal scale.
+  var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    //Create SVG element
-    var svg = d3.select("body")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
-
-    //         var container = d3.select('g') //adjust the position of the <g> so it's in the center of the SVG
-    // .attr('transform', 'translate(' + (w / 3) + ',' + (h / 3) + ')');
+  // Create SVG element.
+  var svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
             
-    // Set up groups.
-    var arcs = svg.selectAll("g.arc")
-      .data(pie(dataset))
-      .enter()
-      .append("g")
-      .attr("class", "arc")
-      .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
+  // Set up groups.
+  var arcs = svg.selectAll("g.arc")
+    .data(pie(dataset))
+    .enter()
+    .append("g")
+    .attr("class", "arc")
+    .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
             
-    // Draw arc paths.
-    arcs.append("path")
-        .attr("fill", function(d, i) {
-          return color(i);
-        })
-        .attr("d", arc);
+  // Draw arc paths.
+  arcs.append("path")
+    .attr("fill", function(d, i) {
+      return color(i);
+    })
+    .attr("d", arc);
             
-    //Labels
-    arcs.append("text")
-        .attr("transform", function(d) {
-          return "translate(" + arc.centroid(d) + ")";
-        })
-        .attr("text-anchor", "middle")
-        .attr('dy', '-20')
-        .style("font", "bold 12px Arial")
-        .text(function(d, i) {
-          return d.value + " incidents\
-        (" + tempData[i].key + ")";
-        });
+  // Labels.
+  arcs.append("text")
+    .attr("transform", function(d) {
+      return "translate(" + arc.centroid(d) + ")";
+    })
+    .attr("text-anchor", "middle")
+    .attr('dy', '-20')
+    .style("font", "bold 14px Arial")
+    .text(function(d, i) {
+        //   return d.value + " incidents\
+        // (" + tempData[i].key + ")";
+      return d.value;
+    });
 
   // Add a legendLabel to each arc slice.
   arcs.append("text")
-    .attr("transform", function(d) { //set the label's origin to the center of the arc
-        //we have to make sure to set these before calling arc.centroid
-        d.outerRadius = outerRadius + 50; // Set Outer Coordinate
-        d.innerRadius = outerRadius + 45; // Set Inner Coordinate
+    .attr("transform", function(d) { // Set the label's origin to the center of the arc.
+        // Set these before calling arc.centroid.
+        d.outerRadius = outerRadius + 50; // Set Outer Coordinate.
+        // d.innerRadius = outerRadius + 45; // Set Inner Coordinate.
         return "translate(" + arc.centroid(d) + ")";
       })
 
-    // .attr("text-anchor", "right") //center the text on it's origin
+    // .attr("text-anchor", "right") // Center the text on it's origin.
       .style("fill", "Purple")
       .style("font", "bold 12px Arial")
-      .text(function(d, i) { return tempData[i].key; }); //get the label from our original data array
+      .text(function(d, i) { return tempData[i].key; }); // Get the label from original data array.
 }); 
